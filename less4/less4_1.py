9b421967a9ed0ece160ad69927ca3c8fae5c64de
ReenTen <meleshko.devilhunter@gmail.com>
2022-04-18 22:53:11 +0300

    добавлено задание 4

---
+++ less4/less4_1.py
@@ -0,0 +1,5 @@
+name = str.capitalize(input('Введите Ваше имя: '))# str.capitalize - введенное имя с верхнего регистра
+age = input('Введите Ваш возраст: ')
+
+full = str(f'Привет, {name}! Тебе {age} лет!')
+print(full)
